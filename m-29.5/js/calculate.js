b3ff056e797401f84e4058c4a6f0ebb7dff769c4
delwerHossain <delwerhossain006@gmail.com>
2023-02-16 15:11:13 +0000

    Total Expense : Total Balance : add

--- m-29.5/js/calculate.js
+++ m-29.5/js/calculate.js
@@ -1,19 +1,23 @@
-document.getElementById("btn-calculate").addEventListener("click", function () { 
+document.getElementById("btn-calculate").addEventListener("click", function () {
     // define all amount in variables 
     const incomeAmountValue = getValue("input-income");
     const foodAmountValue = getValue("input-food");
     const rentAmountValue = getValue("input-rent");
     const clothAmountValue = getValue("input-cloth");
     // const outputAmount = getInnertext("output");
-    const totalBalanceInner = getInnertext("total-balance");
-    const totalCostInner = getInnertext("total-balance");
-    const totalCost = (foodAmount + rentAmount + clothAmount);
+    // const totalBalanceInner = getInnertext("total-cost");
+    // const totalCostInner = getInnertext("total-balance");
+
+    // calculate total cost
+    const totalCostAmount = (foodAmountValue + rentAmountValue + clothAmountValue);
+    document.getElementById("total-cost").innerText = totalCostAmount;
 
 
-    const totalAmount = incomeAmount - totalCost;
-    document.getElementById('output').innerText = totalAmount;
-    
+    // calculate total balance
+    const totalBalanceAmount = incomeAmountValue - totalCostAmount;
+    document.getElementById('total-balance').innerText = totalBalanceAmount;
 
 
- 
+
+
 })
