7c99d10fbb3f822d6c35dde937bfba57caa417ff
delwerHossain <delwerhossain006@gmail.com>
2023-02-16 15:25:55 +0000

    add

--- m-29.5/js/calculate.js
+++ m-29.5/js/calculate.js
@@ -4,6 +4,9 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
     const foodAmountValue = getValue("input-food");
     const rentAmountValue = getValue("input-rent");
     const clothAmountValue = getValue("input-cloth");
+    const saveAmountValue = getValue("input-save");
+
+
     // const outputAmount = getInnertext("output");
     // const totalBalanceInner = getInnertext("total-cost");
     // const totalCostInner = getInnertext("total-balance");
@@ -16,8 +19,14 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
     // calculate total balance
     const totalBalanceAmount = incomeAmountValue - totalCostAmount;
     document.getElementById('total-balance').innerText = totalBalanceAmount;
-
-
-
-
 })
+
+document.getElementById("save").addEventListener("click", function () {
+    
+    // save section 
+    const BalanceInner = getInnertext("total-balance");
+    const remainingBalanceInner = BalanceInner - saveAmountValue;
+    document.getElementById("remaining-balance").innerText = remainingBalanceInner;
+
+    document.getElementById('saving-amount').innerText = saveAmountValue
+});
